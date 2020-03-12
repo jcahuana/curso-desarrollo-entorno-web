d097a2c564210d32a48a3e659dab3aedc14d40c8
unknown <alumnos@A201-16.upc.edu.pe>
2020-03-11 22:12:01 -0500

    Add pages and controllers
    - index about_us faq tos contact_us

--- restapp-sprint1/config/routes.rb
+++ restapp-sprint1/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+  get 'pages/about_us'
+  get 'pages/faq'
+  get 'pages/tos'
+  get 'pages/contact_us'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
